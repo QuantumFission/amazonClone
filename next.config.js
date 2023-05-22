78cfd9c10cc25dd33867708032bc1823feb6a61c
QuantumFission <akashdebnathwd@gmail.com>
2023-05-22 17:21:43 +0530

    update next config

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["links.papareact.com", "fakestoreapi.com", "cdn.discordapp.com","lh3.googleusercontent.com", "https://avatars.githubusercontent.com/", "avatars.githubusercontent.com", "drive.google.com"]
+    domains: ["links.papareact.com", "fakestoreapi.com", "cdn.discordapp.com","lh3.googleusercontent.com", "avatars.githubusercontent.com", "drive.google.com", "amazon-clone-silk-one.vercel.app", "amazon-clone-cftg9tkym-codeandcoffie.vercel.app"]
   },
   env: {
     stripe_public_key: process.env.STRIPE_PUBLIC_KEY
